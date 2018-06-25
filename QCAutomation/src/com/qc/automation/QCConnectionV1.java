286cec3331ce2fe8b57580b2f3d6904485d60251
Robert Jenifer Vilbert <robertarjun46@gmail.com>
2018-06-25 12:13:45 +0530

    updated

--- QCAutomation/src/com/qc/automation/QCConnectionV1.java
+++ QCAutomation/src/com/qc/automation/QCConnectionV1.java
@@ -2,15 +2,22 @@ package com.qc.automation;
 
 import org.apache.log4j.Logger;
 
-
 public class QCConnectionV1 {
 	private static final Logger logger = Logger.getLogger(QCConnectionV1.class);
-	public static void main(String[] args) {
+
+	public void getConnection() {
 		logger.debug("Entering Main Method .. ");
-		QCInitalConnection initial= new QCInitalConnection();
+		QCInitalConnection initial = new QCInitalConnection();
 		initial.doInit();
 		logger.debug("Project Successfully Conneted..");
 		initial.disconnect();
 		logger.debug("Project Successfully DisConneted..");
 	}
+	
+	  public static void main(String[] args) {
+	 logger.debug("Entering Main Method .. "); QCInitalConnection initial= new
+	 QCInitalConnection(); initial.doInit();
+	 logger.debug("Project Successfully Conneted.."); initial.disconnect();
+	 logger.debug("Project Successfully DisConneted.."); }
+	 
 }
